11574d637f94fc660768de1820ed0211867778fc
Agent Hacker <43415449+AgentHackerYT@users.noreply.github.com>
2022-03-16 21:57:19 +0300

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -4,18 +4,26 @@ declare module "agents-fetch"
 
 export type Type = "JSON" | "Buffer" | "Status" | "Text" | "Response"
 
-export function get(url: string, type: Type, options: RequestOptions, callback: (response) => void)
+export function get(url: string, type: Type, options: RequestOptions): Promise<void>;
 
-export function get(url: string, type: "Status", options: RequestOptions, callback: (response: {statusCode: number, statusMessage}) => void)
+export function get(url: string, type: "Status", options: RequestOptions): Promise<{statusCode: number, statusMessage}>;
 
-export function get(url: string, type: "Response", options: RequestOptions, callback: (response: IncomingMessage) => void)
+export function get(url: string, type: "Response", options: RequestOptions): Promise<IncomingMessage>;
 
-export function get(url: string, type: "Text", options: RequestOptions, callback: (response: string) => void)
+export function get(url: string, type: "Text", options: RequestOptions): Promise<string>
 
-export function post(url: string, type: Type, options: RequestOptions, callback: (response) => void)
+export function post(url: string, type: Type, options: RequestOptions): Promise<void>;
 
-export function post(url: string, type: "Status", options: RequestOptions, callback: (response: {statusCode: number, statusMessage}) => void)
+export function post(url: string, type: "Status", options: RequestOptions): Promise<{statusCode: number, statusMessage}>;
 
-export function post(url: string, type: "Response", options: RequestOptions, callback: (response: IncomingMessage) => void)
+export function post(url: string, type: "Response", options: RequestOptions): Promise<IncomingMessage>;
 
-export function post(url: string, type: "Text", options: RequestOptions, callback: (response: string) => void)
+export function post(url: string, type: "Text", options: RequestOptions): Promise<string>
+
+export default function(url: string,type: "Status",options: RequestOptions): Promise<{statusCode: number, statusMessage}>;
+
+export default function(url: string, type: Type, options: RequestOptions): Promise<void>;
+
+export default function(url: string,type: "Response",options: RequestOptions): Promise<IncomingMessage>;
+
+export default function(url: string,type: "Text", options: RequestOptions): Promise<string>
