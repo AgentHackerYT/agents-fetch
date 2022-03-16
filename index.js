98932af12e599b209597756b11c70e6a79231c40
Agent Hacker <43415449+AgentHackerYT@users.noreply.github.com>
2022-03-16 13:26:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
-module.exports.get = require("./methods/get.js")
-module.exports.post = require("./methods/post.js")
+module.exports.get = require('./methods/get')
+
+module.exports.post = require('./methods/post')
